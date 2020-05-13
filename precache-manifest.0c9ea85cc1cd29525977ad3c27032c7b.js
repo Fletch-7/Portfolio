c1269879171d9caa3ebcdcf499cb83db8f20bfc5
Fletch-7 <r.a.m.fletcher7@gmail.com>
2020-05-13 14:54:17 +0100

    Updates

---
+++ precache-manifest.0c9ea85cc1cd29525977ad3c27032c7b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69328b8a58e32d96fe244d39112cb691",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e8c58c3aa0913ff494b5",
+    "url": "/Portfolio/static/css/2.49233dca.chunk.css"
+  },
+  {
+    "revision": "972ca98480d5feab4b13",
+    "url": "/Portfolio/static/css/main.11a073f6.chunk.css"
+  },
+  {
+    "revision": "e8c58c3aa0913ff494b5",
+    "url": "/Portfolio/static/js/2.d26e1e1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.d26e1e1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972ca98480d5feab4b13",
+    "url": "/Portfolio/static/js/main.1815ddd0.chunk.js"
+  },
+  {
+    "revision": "3bad53f1b5e284037689",
+    "url": "/Portfolio/static/js/runtime-main.e4104108.js"
+  },
+  {
+    "revision": "ef1c2c1ba346cc9321496427961400ae",
+    "url": "/Portfolio/static/media/Geocachr.ef1c2c1b.png"
+  },
+  {
+    "revision": "486b18a5465794012add136dac838f67",
+    "url": "/Portfolio/static/media/Hero_API.486b18a5.png"
+  },
+  {
+    "revision": "738d58615aef9fd90110665f277ffd78",
+    "url": "/Portfolio/static/media/Pensive.738d5861.png"
+  },
+  {
+    "revision": "1a2e7ccea2c88192f2120e5a535f252c",
+    "url": "/Portfolio/static/media/Snake_Game.1a2e7cce.png"
+  },
+  {
+    "revision": "890f073ddd086385cb70e25c133afef4",
+    "url": "/Portfolio/static/media/pcosparty.890f073d.png"
+  },
+  {
+    "revision": "d5ceec9a3e70a687ffe3e58f6161e52c",
+    "url": "/Portfolio/static/media/underc.d5ceec9a.png"
+  }
+]);
